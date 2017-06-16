c8204be9b86138576ffe636d8a6a913ad8f0dd3a
Ryosuke Fujimaki <fujimaki@qwintet.com>
2017-06-16 15:21:01 +0900

    調整

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-ln -sd ~/dotfiles/.vim ~/.vim
+if [ ! -L ~/.vim ]; then
+	ln -sd ~/dotfiles/.vim ~/.vim
+fi
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
